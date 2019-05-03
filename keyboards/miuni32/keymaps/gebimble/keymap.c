1b97f6da0dda39978b955b0654f8afafa6b861d6
Joe <joseph.beaver@gmail.com>
2019-05-03 21:06:22 +0100

    Nearly fixed

--- keyboards/miuni32/keymaps/gebimble/keymap.c
+++ keyboards/miuni32/keymaps/gebimble/keymap.c
@@ -8,29 +8,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,---------------------------------------------------------------------------------------.
      * |   Q   |   W   |   E   |   R   |   T   |   Y   |   U   |   I   |   O   |   P   |  BSP  |
      * |---------------------------------------------------------------------------------------|
-     * |   A   |   S   |   D   |   F   |   G   |   H   |   J   |   K   |   L   |  ENT  |LT(1|;)|
+     * |LT(2|A)|   S   | ALT/D | CTL/F |   G   |   H   | CTL/J | ALT/K |   L   |SPC/SHF|LT(1|;)|
      * |---------------------------------------------------------------------------------------|
-     * |LT(3|Z)|   X   |   C   |   V   |      SPC      |   B   |   N   |   M   |   ,   |LT(2|.)|
+     * |LT(3|Z)|   X   |   C   |   V   |   B   |   N   |   M   |   ,   |   .   |   /   |  ENT  |
      * |---------------------------------------------------------------------------------------|
      */
-  [0] = LAYOUT(
-    KC_Q,        KC_W, KC_E, KC_R, KC_T, KC_Y,   KC_U, KC_I, KC_O, KC_P,    KC_BSPC,
-    KC_A,        KC_S, KC_D, KC_F, KC_G, KC_H,   KC_J, KC_K, KC_L, KC_ENT,  LT(1, KC_SCLN),
-    LT(3, KC_Z), KC_X, KC_C, KC_V, SFT_T(KC_SPC),KC_B, KC_N, KC_M, KC_COMM, LT(2, KC_DOT)
+  [0] = LAYOUT_ortho_3x11(
+    KC_Q,        KC_W, KC_E,       KC_R,       KC_T, KC_Y, KC_U,       KC_I,       KC_O,    KC_P,          KC_BSPC,
+    LT(2, KC_A), KC_S, LALT(KC_D), LCTL(KC_F), KC_G, KC_H, RCTL(KC_J), RALT(KC_K), KC_L,    SFT_T(KC_SPC), LT(1, KC_SCLN),
+    LT(3, KC_Z), KC_X, KC_C,       KC_V,       KC_B, KC_N, KC_M,       KC_COMM,    KC_DOT,  KC_SLSH,       KC_ENT)
   ),
   /* Level 1: Numbers Layer
      * ,---------------------------------------------------------------------------------------.
-     * |  ESC  |   7   |   8   |   9   |   /   |   *   |   .   |   ,   |   (   |   )   |  DEL  |
+     * |   1   |   2   |   3   |   4   |   5   |   6   |   7   |   8   |   9   |   0   |  DEL  |
      * |---------------------------------------------------------------------------------------|
-     * |  TAB  |   4   |   5   |   6   |   -   |   +   |  HOME |   UP  |  END  |  PGUP |  TRNS |
+     * |  TAB  |   {   |   }   |   [   |   ]   |  TRNS |   -   |   =   |  TRNS |  TRNS |  TRNS |
      * |---------------------------------------------------------------------------------------|
-     * |  LATL |   1   |   2   |   3   |   0           |  LEFT |  DOWN |  RGHT |  PGDN | RSHFT |
+     * |   \   |   `   |  GUI  |  TRNS |  TRNS |  TRNS |  TRNS |  TRNS |  TRNS |   #   |   /   |
      * |---------------------------------------------------------------------------------------|
      */
-  [1] = LAYOUT(
-    KC_ESC,  KC_7, KC_8, KC_9, KC_SLSH, KC_ASTR, KC_DOT,  KC_COMM, KC_LPRN, KC_RPRN, KC_DEL,
-    KC_TAB,  KC_4, KC_5, KC_6, KC_MINS, KC_PLUS, KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______,
-    KC_LALT, KC_1, KC_2, KC_3, KC_0,             KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_RSFT
+  [1] = LAYOUT_ortho_3x11(
+    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_DEL,
+    KC_TAB,  KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, _______, KC_MINS, KC_EQL,  _______, _______, _______,
+    KC_NUBS, KC_GRV,  KC_LGUI, _______, _______, _______, _______, _______, _______, KC_NUHS, KC_SLSH
   ),
   /* Level 2: Symbols Layer
      * ,---------------------------------------------------------------------------------------.
@@ -38,13 +38,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |---------------------------------------------------------------------------------------|
      * |   {   |   }   |   [   |   ]   |  TRNS |  TRNS |   \   |   #   |   _   |   `   |   ?   |
      * |---------------------------------------------------------------------------------------|
-     * |  LSFT |  LCTL |   L   |   T   |      TAB      |   N   |  TRNS |  TRNS |  RCTL |  TRNS |
+     * |  LSFT |  LCTL |   L   |   T   |       |  TAB  |   N   |  TRNS |  TRNS |  RCTL |  TRNS |
      * |---------------------------------------------------------------------------------------|
      */
-  [2] = LAYOUT(
+  [2] = LAYOUT_ortho_3x11(
     KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_PLUS, KC_EQL,
     KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, _______, _______, KC_NUBS, KC_NUHS, KC_UNDS, KC_GRV,  KC_QUOT,
-    KC_LSFT, KC_LCTL, KC_L,    KC_T,    SFT_T(KC_SPC)   ,  KC_N,    _______, _______, KC_RCTL, _______
+    KC_LSFT, KC_LCTL, KC_L,    KC_T,    _______, KC_TAB,  KC_N,    _______, _______, KC_RCTL, _______
   ),
   /* Level 3: RGB Layer
      * ,---------------------------------------------------------------------------------------.
@@ -52,13 +52,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |---------------------------------------------------------------------------------------|
      * |RGB_TOG|RGB_MOD|RGB_HUI|RGB_HUD|   NO  |RGB_SAI|RGB_SAD|RGB_VAI|RGB_VAD|  TRNS |  TRNS |
      * |---------------------------------------------------------------------------------------|
-     * |  TRNS |  TRNS |  TRNS |  TRNS |       F7      |   F8  |   F9  |  F10  |  F11  |  F12  |
+     * |  CAPS |  TRNS |  TRNS |  TRNS |       |   F7  |   F8  |   F9  |  F10  |  F11  |  F12  |
      * |---------------------------------------------------------------------------------------|
      */
-  [3] = LAYOUT(
+  [3] = LAYOUT_ortho_3x11(
     RESET,   _______, _______, _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,
     RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, KC_NO,   RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______,
-    _______, _______, _______, _______,          KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12
+    KC_CAPS, _______, _______, _______, _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12
   )
 };
 
