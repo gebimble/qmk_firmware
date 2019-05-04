6098873db2f4bbd4c99b1dbd70acb04ec9462b44
Joe <joseph.beaver@gmail.com>
2019-05-04 23:42:50 +0100

    final keymap configuration (for now); shifted layer 1&2 toggle to b&n

--- keyboards/miuni32/keymaps/gebimble/keymap.c
+++ keyboards/miuni32/keymaps/gebimble/keymap.c
@@ -21,42 +21,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,---------------------------------------------------------------------------------------.
      * |   Q   |   W   |   E   |   R   |   T   |   Y   |   U   |   I   |   O   |   P   |  BSP  |
      * |---------------------------------------------------------------------------------------|
-     * |LT(S|A)|   S   | ALT/D | CTL/F |   G   |   H   | CTL/J | ALT/K |   L   |SHF/SPC|LT(1|;)|
+     * |   A   |   S   | ALT/D | CTL/F |   G   |   H   | CTL/J | ALT/K |   L   |SHF/SPC|   ;   |
      * |---------------------------------------------------------------------------------------|
-     * | SHF/Z |   X   |   C   |   V   |   B   |   N   |   M   |   ,   |   .   |   /   |  ENT  |
+     * | SHF/Z |   X   |   C   |   V   |LT(@|B)|LT(#|N)|   M   |   ,   |   .   |   /   |LT(A|↵)|
      * |---------------------------------------------------------------------------------------|
      */
   [_QWERTY] = LAYOUT_ortho_3x11(
-    KC_Q,               KC_W, KC_E,         KC_R,         KC_T, KC_Y, KC_U,         KC_I,         KC_O,   KC_P,          KC_BSPC,
-    LT(_SYMBOLS, KC_A), KC_S, LALT_T(KC_D), LCTL_T(KC_F), KC_G, KC_H, RCTL_T(KC_J), RALT_T(KC_K), KC_L,   SFT_T(KC_SPC), LT(_NUMBERS, KC_SCLN),
-    SFT_T(KC_Z),        KC_X, KC_C,         KC_V,         KC_B, KC_N, KC_M,         KC_COMM,      KC_DOT, KC_SLSH,       LT(_ADJUST, KC_ENT)
+    KC_Q,         KC_W, KC_E,         KC_R,         KC_T,               KC_Y,               KC_U,         KC_I,         KC_O,   KC_P,          KC_BSPC,
+    KC_A,         KC_S, LALT_T(KC_D), LCTL_T(KC_F), KC_G,               KC_H,               RCTL_T(KC_J), RALT_T(KC_K), KC_L,   SFT_T(KC_SPC), KC_SCLN,
+    SFT_T(KC_Z),  KC_X, KC_C,         KC_V,         LT(_SYMBOLS, KC_B), LT(_NUMBERS, KC_N), KC_M,        KC_COMM,      KC_DOT, KC_SLSH,       LT(_ADJUST, KC_ENT)
   ),
     /* Workman
      * ,---------------------------------------------------------------------------------------.
      * |   Q   |   D   |   R   |   W   |   B   |   J   |   F   |   U   |   P   |   ;   |  BSP  |
      * |---------------------------------------------------------------------------------------|
-     * |LT(2|A)|   S   | ALT/H | CTL/T |   G   |   Y   | CTL/N | ALT/E |   O   |SHF/SPC|LT(1|I)|
+     * |   A   |   S   | ALT/H | CTL/T |   G   |   Y   | CTL/N | ALT/E |   O   |SHF/SPC|   I   |
      * |---------------------------------------------------------------------------------------|
-     * | SHF/Z |   X   |   M   |   C   |   V   |   K   |   L   |   ,   |   .   |   /   |LTENT  |
+     * | SHF/Z |   X   |   M   |   C   |LT(@|V)|LT(#|K)|   L   |   ,   |   .   |   /   |LT(A|↵)|
      * |---------------------------------------------------------------------------------------|
      */
   [_WORKMAN] = LAYOUT_ortho_3x11(
-    KC_Q,               KC_D, KC_R,         KC_W,         KC_B, KC_J, KC_F,         KC_U,         KC_P,   KC_SCLN,       KC_BSPC,
-    LT(_SYMBOLS, KC_A), KC_S, LALT_T(KC_H), LCTL_T(KC_T), KC_G, KC_Y, RCTL_T(KC_N), RALT_T(KC_E), KC_O,   SFT_T(KC_SPC), LT(_NUMBERS, KC_I),
-    SFT_T(KC_Z),        KC_X, KC_M,         KC_C,         KC_V, KC_K, KC_L,         KC_COMM,      KC_DOT, KC_SLSH,       LT(_ADJUST, KC_ENT)
+    KC_Q,        KC_D, KC_R,         KC_W,         KC_B, KC_J,                               KC_F,         KC_U,         KC_P,   KC_SCLN,       KC_BSPC,
+    KC_A,        KC_S, LALT_T(KC_H), LCTL_T(KC_T), KC_G, KC_Y,                               RCTL_T(KC_N), RALT_T(KC_E), KC_O,   SFT_T(KC_SPC), KC_I,
+    SFT_T(KC_Z), KC_X, KC_M,         KC_C,         LT(_SYMBOLS, KC_V),  LT(_NUMBERS,  KC_K), KC_L,         KC_COMM,      KC_DOT, KC_SLSH,       LT(_ADJUST, KC_ENT)
   ),
   /* Numbers Layer
      * ,---------------------------------------------------------------------------------------.
      * |   1   |   2   |   3   |   4   |   5   |   6   |   7   |   8   |   9   |   0   |  TRNS |
      * |---------------------------------------------------------------------------------------|
-     * |  TAB  |   [   |   ]   |  TRNS |  TRNS |  TRNS |   -   |   =   |  TRNS |  TRNS |  TRNS |
+     * |  TAB  |   [   |   ]   |  ESC  |  TRNS |  TRNS |   -   |   =   |  TRNS |  TRNS |  TRNS |
      * |---------------------------------------------------------------------------------------|
      * |   \   |   `   |  GUI  |   #   |   /   |  TRNS |  TRNS |  TRNS |  TRNS |  TRNS |  TRNS |
      * |---------------------------------------------------------------------------------------|
      */
   [_NUMBERS] = LAYOUT_ortho_3x11(
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-    KC_TAB,  KC_LBRC, KC_RBRC, _______, _______, _______, KC_MINS, KC_EQL,  _______, _______, _______,
+    KC_TAB,  KC_LBRC, KC_RBRC, KC_ESC,  _______, _______, KC_MINS, KC_EQL,  _______, _______, _______,
     KC_NUBS, KC_GRV,  KC_LGUI, KC_NUHS, KC_SLSH, _______, _______, _______, _______, _______, _______
   ),
   /* Symbols Layer
@@ -65,13 +65,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |---------------------------------------------------------------------------------------|
      * |  TRNS |  TRNS |  TRNS |  TRNS |  TRNS |  TRNS |   _   |   +   |   {   |   }   |   '   |
      * |---------------------------------------------------------------------------------------|
-     * |  TRNS |  TRNS |  TRNS |  SHFT |  ALT  |  TAB  |  ESC  |   |   |   ¬   |   ~   |   @   |
+     * |  TRNS |  TRNS |  TRNS |  TRNS |  TRNS |  TRNS |  TRNS |   |   |   ¬   |   ~   |   @   |
      * |---------------------------------------------------------------------------------------|
      */
   [_SYMBOLS] = LAYOUT_ortho_3x11(
     KC_EXLM, LSFT(KC_2), LSFT(KC_3), KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR,       KC_LPRN,      KC_RPRN, KC_DEL,
     _______, _______,    _______,    _______, _______, _______, KC_UNDS, KC_PLUS,       KC_LCBR,      KC_RCBR, KC_QUOT,
-    _______, _______,    _______,    KC_LSFT, KC_LALT, KC_TAB,  KC_ESC,  LSFT(KC_NUBS), LSFT(KC_GRV), KC_TILD, LSFT(KC_QUOT)
+    _______, _______,    _______,    _______, _______, _______, _______, LSFT(KC_NUBS), LSFT(KC_GRV), KC_TILD, LSFT(KC_QUOT)
   ),
   /* Level 3: RGB Layer
      * ,---------------------------------------------------------------------------------------.
